560cf788e26381399112a8e2c14635ec28b35d37
robertguides <guidesjohnrobert@gmail.com>
2017-10-05 15:24:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/guides/alertdialog2/CustomDialog.java
@@ -0,0 +1,54 @@
+package com.example.guides.alertdialog2;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by GUIDES on 10/5/2017.
+ */
+
+public class CustomDialog extends DialogFragment {
+
+    LayoutInflater inflater;
+    View view;
+    EditText etUser, etPwd;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        inflater = getActivity().getLayoutInflater();
+         view = inflater.inflate(R.layout.login_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view);
+        builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                etUser = (EditText) view.findViewById(R.id.et_username);
+                etPwd = (EditText) view.findViewById(R.id.et_password);
+                String user = etUser.getText().toString();
+                String password= etPwd.getText().toString();
+                String msg = "Username is " + user + ", Password is " + password ;
+                Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+        dialogInterface.cancel();
+        }
+    });
+
+
+        builder.setTitle("Ako ay Login Bar");
+        return builder.create();
+    }
+}
